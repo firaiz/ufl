c4a6b36f9d964f41dbcadbb7f6df57c470c03a0a
romer <romer.t.co@gmail.com>
2015-12-10 19:18:33 +0900

    storageのヘルパー関数追加

---
+++ src/helpers.php
@@ -0,0 +1,10 @@
+<?php
+
+use AnySys\Storage;
+
+if (!function_exists('storage')) {
+    function storage($key, $isCreate = false) {
+        return Storage::getInstance()->getPath($key, $isCreate);
+    }
+}
+

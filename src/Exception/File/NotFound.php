97bfe584cc5c7d98eea095d74078c431bf541aee
romer <romer.t.co@gmail.com>
2015-12-10 17:30:58 +0900

    Exceptionファイルを外部化

---
+++ src/Exception/File/NotFound.php
@@ -0,0 +1,12 @@
+<?php
+namespace AnySys\Exception\File;
+
+use Exception;
+
+class NotFound extends Exception
+{
+    public function __construct($message = "", $code = 0, Exception $previous = null)
+    {
+        parent::__construct("file not found", 404, $previous);
+    }
+}

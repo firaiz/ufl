93a5bf66f8b5e13786c259aba6580980473489db
kenichi yoshida <yoshida@anysys.jp>
2016-09-02 11:00:47 +0900

    fix holiday class comment

--- src/Holiday.php
+++ src/Holiday.php
@@ -1,6 +1,7 @@
 <?php
 namespace UflAs;
 
+use UflAs\Holiday\Holiday AS HolidayBase;
 use UflAs\Holiday\Japan AS JapanHoliday;
 
 class Holiday
@@ -8,7 +9,7 @@ class Holiday
     /**
      * @param int $year
      * @param string [$countryCode]
-     * @return static[]|array
+     * @return HolidayBase[]|array
      */
     public static function listOf($year, $countryCode = 'JP') {
         switch ($countryCode) {

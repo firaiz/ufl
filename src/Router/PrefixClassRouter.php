6acd4cffc4f2555696852c549553d4b33e6a3758
romer <romer.t.co@gmail.com>
2017-11-20 11:04:17 +0900

    prefix router追加

---
+++ src/Router/PrefixClassRouter.php
@@ -0,0 +1,23 @@
+<?php
+namespace UflAs\Router;
+
+class PrefixClassRouter extends ClassRouter
+{
+    protected $prefix = null;
+
+    public function setPrefix($prefix) {
+        $this->prefix = $prefix;
+    }
+
+    public function getPathInfo()
+    {
+        $pathInfo = parent::getPathInfo();
+        if (is_null($this->prefix)) {
+            return $pathInfo;
+        }
+        if (strpos($pathInfo, $this->prefix) !== 0) {
+            return '/';
+        }
+        return preg_replace('#^'.$this->prefix.'#', '', $pathInfo);
+    }
+}

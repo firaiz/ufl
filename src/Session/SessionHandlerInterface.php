ce6dcd10b814f32251bb2d73eab3240da08b2562
kenichi yoshida <yoshida@anysys.jp>
2017-11-20 19:04:45 +0900

    SessionHandlerInterfaceの定義の仕方を変更

--- src/Session/SessionHandlerInterface.php
+++ src/Session/SessionHandlerInterface.php
@@ -1,14 +1,7 @@
 <?php
 namespace UflAs\Session;
 
-if (!defined('PHP_VERSION_ID')) {
-    $version = explode('.', PHP_VERSION);
-    define('PHP_VERSION_ID', ($version[0] * 10000 + $version[1] * 100 + $version[2]));
-}
-if (!defined('IS_LEGACY_PHP')) {
-    define('IS_LEGACY_PHP', PHP_VERSION_ID < 50400);
-}
-if (IS_LEGACY_PHP) {
+if (!interface_exists('SessionHandlerInterface')) {
     interface SessionHandlerInterface
     {
         public function close();

86020dc72d7a2141af9120830cc1f18ec0e6a45c
romer <romer.t.co@gmail.com>
2017-11-20 11:02:13 +0900

    cache追加

---
+++ src/Cache/Generator.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace UflAs\Cache;
+
+use Doctrine\Common\Cache\CacheProvider;
+use ReflectionClass;
+
+class Generator
+{
+    /**
+     * @param Initializer $initializer
+     * @return CacheProvider
+     * @throws ClassNotFound
+     */
+    public static function generate($initializer)
+    {
+        $callParams = call_user_func($initializer->getParamGenerator());
+        $className = '\\Doctrine\\Common\\Cache\\'.$initializer->getCacheType().'Cache';
+        if (!class_exists($className)) {
+            throw new ClassNotFound();
+        }
+
+        if (!is_array($callParams)) {
+            return new $className();
+        }
+
+        $reflection = new ReflectionClass($className);
+        return $reflection->newInstanceArgs($callParams);
+    }
+}
---
+++ src/Cache/Initializer.php
@@ -0,0 +1,40 @@
+<?php
+namespace UflAs\Cache;
+
+class Initializer
+{
+    const CACHE_TYPE_SQLite3 = 'SQLite3';
+
+    /** @var Callable */
+    private $paramGenerator;
+    /** @var string */
+    private $cacheType;
+
+    public function __construct($callable, $cacheType = self::CACHE_TYPE_SQLite3)
+    {
+        $this->paramGenerator = $callable;
+        $this->cacheType = $cacheType;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCacheType()
+    {
+        return $this->cacheType;
+    }
+
+    /**
+     * Doctrine Cache initial parameters
+     * @return Callable
+     *
+     * callable example for SQLite cache
+     * function () use($path) {
+     *  return [new SQLite3($path), 'exampleCache'];
+     * }
+     */
+    public function getParamGenerator()
+    {
+        return $this->paramGenerator;
+    }
+}

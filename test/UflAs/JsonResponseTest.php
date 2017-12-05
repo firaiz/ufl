d17f06f69c185bcf419c967484cb25127b8c37ec
romer <romer.t.co@gmail.com>
2017-12-05 09:53:20 +0900

    Jsonレスポンスのテスト追加

---
+++ test/UflAs/JsonResponseTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace UflAs;
+
+include_once __DIR__ . '/../../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+
+class JsonResponseTest extends TestCase
+{
+    /**
+     * @runInSeparateProcess
+     */
+    public function testOne()
+    {
+        $jsonResponse = JsonResponse::getInstance();
+        $jsonResponse->assign('a', 'b');
+        ob_start();
+        $jsonResponse->write();
+        $result = ob_get_clean();
+        $this->assertEquals($result, '{"a":"b"}');
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testObject()
+    {
+        $jsonResponse = JsonResponse::getInstance();
+
+        $obj = new \stdClass();
+        $obj->a = "1";
+        $obj->b = 2;
+        $obj->c = array(1,2,3,4,5);
+
+        $jsonResponse->assign('a', $obj);
+        ob_start();
+        $jsonResponse->write();
+        $result = ob_get_clean();
+        $this->assertEquals($result, '{"a":{"a":"1","b":2,"c":[1,2,3,4,5]}}');
+    }
+}

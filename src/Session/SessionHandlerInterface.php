283d5bae724a041ae67afbcdefad6dbc517aab18
kenichi yoshida <yoshida@anysys.jp>
2018-03-13 15:41:16 +0900

    session handlerを別で定義

--- src/Session/SessionHandlerInterface.php
+++ src/Session/SessionHandlerInterface.php
@@ -1,28 +1,12 @@
 <?php
+
 namespace UflAs\Session;
 
-if (!interface_exists('SessionHandlerInterface')) {
-    /**
-     * Interface SessionHandlerInterface
-     * @package UflAs\Session
-     * @covers
-     */
-    interface SessionHandlerInterface
-    {
-        public function close();
-        public function destroy($session_id);
-        public function gc($maxlifetime);
-        public function open($save_path, $name);
-        public function read($session_id);
-        public function write($session_id, $session_data);
-    }
-} else {
-    /**
-     * Interface SessionHandlerInterface
-     * @package UflAs\Session
-     */
-    interface SessionHandlerInterface extends \SessionHandlerInterface
-    {
-        // empty
-    }
+/**
+ * Interface SessionHandlerInterface
+ * @package UflAs\Session
+ */
+interface SessionHandlerInterface extends \SessionHandlerInterface
+{
+    // empty
 }

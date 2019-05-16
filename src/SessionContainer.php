74c825698784a4601146be2a76a30f246da9e620
kenichi yoshida <yoshida@anysys.jp>
2019-05-16 17:41:32 +0900

    SessionContainerのデータ部分とロジック部分を分離

--- src/SessionContainer.php
+++ src/SessionContainer.php
@@ -1,106 +1,15 @@
 <?php
+
 namespace UflAs;
 
+use UflAs\Container\SessionContainer as newSessionContainer;
 
-class SessionContainer
+/**
+ * Class SessionContainer
+ * @package UflAs
+ * @deprecated
+ */
+class SessionContainer extends newSessionContainer
 {
-    /** @var string */
-    protected $prefix;
 
-    /** @var array */
-    protected $container;
-
-    /**
-     * Session constructor.
-     * @param string $prefix
-     */
-    public function __construct($prefix = null)
-    {
-        if (is_string($prefix)) {
-            $this->prefix = $prefix;
-        }
-    }
-
-    /**
-     * unset session
-     * @param string $name
-     */
-    public function del($name = null)
-    {
-        $container =& $this->getContainer();
-        if (is_null($name)) {
-            $container = array();
-            return;
-        }
-
-        $keys = ArrayUtil::toKeys($name);
-        $key = $name;
-        if (count($keys) === 1) {
-            if (array_key_exists($key, $container)) {
-                unset($container[$key]);
-            }
-            return;
-        }
-
-        $lastKey = array_pop($keys);
-        $key = ArrayUtil::toKey($keys);
-        $setValue = ArrayUtil::get($container, $key, array());
-        if (array_key_exists($lastKey, $setValue)) {
-            unset($setValue[$lastKey]);
-        }
-
-        $this->set($key, $setValue);
-    }
-
-    /**
-     * @name string
-     * @return array
-     */
-    protected function &getContainer()
-    {
-        if (!is_array($this->container)) {
-            $this->initContainer();
-        }
-        return $this->container;
-    }
-
-    /**
-     * init session container
-     */
-    protected function initContainer()
-    {
-        $session =& $this->makeContainer();
-        if (is_string($this->prefix)) {
-            if (!array_key_exists($this->prefix, $session)) {
-                $session[$this->prefix] = array();
-            }
-            $this->container =& $session[$this->prefix];
-            return;
-        }
-        $this->container =& $session;
-    }
-
-    protected function &makeContainer() {
-        return Session::getInstance()->getContainer();
-    }
-
-    /**
-     * @param string $name
-     * @param string $value
-     */
-    public function set($name, $value)
-    {
-        ArrayUtil::set($this->getContainer(), $name, $value);
-    }
-
-    /**
-     * get session data
-     * @param $name
-     * @param mixed $default
-     * @return mixed
-     */
-    public function get($name, $default = null)
-    {
-        return ArrayUtil::get($this->getContainer(), $name, $default);
-    }
 }
